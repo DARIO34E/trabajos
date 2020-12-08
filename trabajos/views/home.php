2797a294d7a23fdeb7c86de965f1ed9cefb57386
DARIO34E <64812170+DARIO34E@users.noreply.github.com>
2020-12-07 19:54:18 -0500

    Add files via upload

---
+++ trabajos/views/home.php
@@ -0,0 +1,11 @@
+<main class="container">
+	<section>
+		<div>
+			<center>
+			<h1 >Bienvenidos Aqui podras Encontrar <br>
+					peliculas de diferentes categorias.....</h1>
+			</center>
+		</div>
+	</section>
+</main>
+</body>
